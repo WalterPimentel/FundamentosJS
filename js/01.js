f3935e5be3f915e492b3cfc4a90065eab4c089c1
WalterPimentel <70056787@continental.edu.pe>
2023-09-22 12:47:51 -0500

    variables let

--- js/01.js
+++ js/01.js
@@ -1,1 +1,17 @@
 // Variables - var let const
+
+let cliente = "Juan" // las variables let se pueden reaccionar el tipo de dato ya sea string, int, etc
+let precioDescuento = 220 //cameCase usar segundo palabra iniciando con mayuscula
+
+//Re asiganar
+cliente = 20
+cliente = true
+
+//Pueden iniciar sin un valor
+
+let precio
+
+console.log(precio) // saldra undfined
+
+console.log(cliente)
+console.log(precioDescuento)
