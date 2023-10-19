371b9b118312826a9ac3c840416a75127b89a52c
WalterPimentel <70056787@continental.edu.pe>
2023-10-19 15:46:47 -0500

    Introducción a Arrays

---
+++ js/08.js
@@ -0,0 +1,12 @@
+// Arrays o Arreglos
+
+const tecnologias = [20, 30, true, 'React', "JavaScript"]
+
+console.log(tecnologias[4])
+console.table(tecnologias)
+console.log("")
+
+console.log(tecnologias.length)
+console.log("")
+
+console.log(tecnologias.toString())
