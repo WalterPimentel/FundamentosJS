e95e1c21207fed39abd6d8592f19cc59a0d89bd1
WalterPimentel <70056787@continental.edu.pe>
2023-10-19 17:24:09 -0500

    Destructruing de Arrays

---
+++ js/10.js
@@ -0,0 +1,7 @@
+/* Destructruing de Arrays */
+
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js']
+
+const [posicion0, posicion1, , , ultimo] = tecnologias
+
+console.log(posicion0, posicion1, ultimo)
