da4f3b56a09a8132c601d9f22b176e56954c3216
WalterPimentel <70056787@continental.edu.pe>
2023-10-20 13:00:04 -0500

    Funciones - Function Declaration

---
+++ js/12.js
@@ -0,0 +1,9 @@
+/* Funciones - Function Declaration */
+
+sumar() // Puedes llamar y ejecutar la funcion desde cualquier parte del código
+
+function sumar() {
+    console.log(2 + 2)
+}
+
+sumar() // Puedes llamar y ejecutar la funcion desde cualquier parte del código
