c8446c45a301680866458d3aa69a4b2798a7b561
WalterPimentel <70056787@continental.edu.pe>
2023-10-20 14:39:13 -0500

    Funciones - Arrow Functions

---
+++ js/14.js
@@ -0,0 +1,12 @@
+/* Funciones - Arrow Functions */
+
+const sumar = (numero, numero2) => numero + numero2 // cuando veas que es '() =>' es por que es un Arrow Function
+
+const sumaArrow = () => 2 + 2
+
+const resultado = sumar(30, 30)
+console.log(resultado)
+console.log("")
+
+const resultado2 = sumaArrow()
+console.log(resultado2)
