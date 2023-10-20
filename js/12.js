07b7910f95a1b09eaa4c8e16d8f0e732cbc36449
WalterPimentel <70056787@continental.edu.pe>
2023-10-20 13:21:39 -0500

    Funciones que Retornan Valores

--- js/12.js
+++ js/12.js
@@ -1,10 +1,23 @@
 /* Funciones - Function Declaration */
 
-function sumar(numero, numero2 = 0) { // lo que está dentro del parentesis se llama parámetros
-    console.log(numero + numero2)
+// function sumar(numero, numero2 = 0) { // lo que está dentro del parentesis se llama parámetros
+//     console.log(numero + numero2)
+// }
+
+// sumar(10, 20) // los valores dentro del parentesis son los argumentos
+// sumar(2, 3)
+// sumar(100)
+// sumar()
+
+/* Funciones que Retornan Valores */
+
+function sumar(numero, numero2 = 0) {
+    return {
+        resultado: numero + numero2, 
+        saludo: 'Hola Mundo'
+    }
 }
 
-sumar(10, 20) // los valores dentro del parentesis son los argumentos
-sumar(2, 3)
-sumar(100)
-sumar()
+const {resultado, saludo} = sumar(20, 30)
+console.log(resultado)
+console.log(saludo)
