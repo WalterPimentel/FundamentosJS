ef05afb4ed6299b2f4d73bca6e4a4e11f01a3ce4
WalterPimentel <70056787@continental.edu.pe>
2023-10-20 21:48:00 -0500

    Condicionales - Condición || y &&

---
+++ js/19.js
@@ -0,0 +1,8 @@
+/* Condicionales - Condición || y && */
+
+const saldo = 600
+const pagar = 500
+const tarjeta = true
+
+if (saldo > pagar && tarjeta) console.log('Puedes pagar')
+else console.log('No, no puedes pagar')
